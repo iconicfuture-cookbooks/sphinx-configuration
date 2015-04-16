4ae81a3e98112faf61b586e4439cb9004b698cf4
Thomas Liebscher <thomas.liebscher@iconcifuture.com>
2015-04-16 17:37:45 +0200

    CPB-509 remove cookbook prefix when including local recipes

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer       "Thomas Liebscher"
 license          "Apache Software License 2.0"
 description      "Configure sphinx search index"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1"
 
 depends "mysql"
 depends "sphinx"
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,4 @@
 # Deploy and configure Iconicfuture licensing platform
 #
 
-include_recipe "sphinx-configuration::configure"
+include_recipe "configure"
